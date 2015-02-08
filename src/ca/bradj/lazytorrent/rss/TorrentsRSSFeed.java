566192e303499be438be8b71add5dba466f71a9b
brad <bradsk88@gmail.com>
2015-02-08 14:06:06 -0600

    Fixed pom.  Skipping description tags.

--- src/ca/bradj/lazytorrent/rss/TorrentsRSSFeed.java
+++ src/ca/bradj/lazytorrent/rss/TorrentsRSSFeed.java
@@ -29,213 +29,216 @@ import com.google.common.collect.Lists;
 
 public class TorrentsRSSFeed implements RSSFeed {
 
-	private final List<RSSUpdateListener> listeners = Lists.newArrayList();
+    private final List<RSSUpdateListener> listeners = Lists.newArrayList();
 
-	private URL url;
+    private URL url;
 
-	private final AlreadyDownloaded already;
+    private final AlreadyDownloaded already;
 
-	private final Logger logger;
+    private final Logger logger;
 
-	private static final String ITEM = "item";
+    private static final String ITEM = "item";
 
-	private static final String CHAN = "channel";
+    private static final String CHAN = "channel";
 
-	private static final Extension NAME_EXT = new Extension() {
-		@Override
-		public Void apply(DefaultRSSTorrent.Builder b, String extension) {
-			b.nameExtend(extension);
-			return null;
-		}
-	};
+    private static final Extension NAME_EXT = new Extension() {
+        @Override
+        public Void apply(DefaultRSSTorrent.Builder b, String extension) {
+            b.nameExtend(extension);
+            return null;
+        }
+    };
 
-	private static final Extension LINK_EXT = new Extension() {
-		@Override
-		public Void apply(DefaultRSSTorrent.Builder b, String extension) {
-			b.linkExtend(extension);
-			return null;
-		}
-	};
+    private static final Extension LINK_EXT = new Extension() {
+        @Override
+        public Void apply(DefaultRSSTorrent.Builder b, String extension) {
+            b.linkExtend(extension);
+            return null;
+        }
+    };
 
-	private static final Extension DESC_EXT = new Extension() {
-		@Override
-		public Void apply(DefaultRSSTorrent.Builder b, String extension) {
-			b.descriptionExtend(extension);
-			return null;
-		}
-	};
+    private static final Extension DESC_EXT = new Extension() {
+        @Override
+        public Void apply(DefaultRSSTorrent.Builder b, String extension) {
+            b.descriptionExtend(extension);
+            return null;
+        }
+    };
 
-	private static final Failable<RSSTorrent> EXPECTED_START_TAG = Failable.fail("Expected start tag");
+    private static final Failable<RSSTorrent> EXPECTED_START_TAG = Failable.fail("Expected start tag");
 
-	public TorrentsRSSFeed(String torrentsURL, AlreadyDownloaded alreadyDownloaded, Logger logger) {
-		this.already = alreadyDownloaded;
-		this.logger = logger;
-		try {
-			this.url = new URL(torrentsURL);
-		} catch (MalformedURLException e) {
-			logger.error("Exception: " + e.getMessage());
-			throw new RuntimeException(e);
-		}
-	}
+    public TorrentsRSSFeed(String torrentsURL, AlreadyDownloaded alreadyDownloaded, Logger logger) {
+        this.already = alreadyDownloaded;
+        this.logger = logger;
+        try {
+            this.url = new URL(torrentsURL);
+        } catch (MalformedURLException e) {
+            logger.error("Exception: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
 
-	@Override
-	public Collection<RSSTorrent> requestRefresh() {
-		Collection<RSSTorrent> t = getNewTorrents();
-		t = strip(t, already);
-		logger.debug(t.size() + " torrents read from server");
-		updateListeners(t);
-		return t;
-	}
+    @Override
+    public Collection<RSSTorrent> requestRefresh() {
+        Collection<RSSTorrent> t = getNewTorrents();
+        t = strip(t, already);
+        logger.debug(t.size() + " torrents read from server");
+        updateListeners(t);
+        return t;
+    }
 
-	protected ObservableList<RSSTorrent> strip(Collection<RSSTorrent> torrents, AlreadyDownloaded alreadyDownloaded2) {
-		ObservableList<RSSTorrent> list = FXCollections.observableArrayList();
-		for (RSSTorrent i : torrents) {
-			if (alreadyDownloaded2.hasSameNameAndEpisode(i)) {
-				continue;
-			}
-			list.add(i);
-		}
-		return list;
-	}
+    protected ObservableList<RSSTorrent> strip(Collection<RSSTorrent> torrents, AlreadyDownloaded alreadyDownloaded2) {
+        ObservableList<RSSTorrent> list = FXCollections.observableArrayList();
+        for (RSSTorrent i : torrents) {
+            if (alreadyDownloaded2.hasSameNameAndEpisode(i)) {
+                continue;
+            }
+            list.add(i);
+        }
+        return list;
+    }
 
-	private void updateListeners(Collection<RSSTorrent> t) {
-		Preconditions.checkNotNull(t);
+    private void updateListeners(Collection<RSSTorrent> t) {
+        Preconditions.checkNotNull(t);
 
-		ImmutableList<RSSTorrent> copyof = ImmutableList.copyOf(t);
-		for (RSSUpdateListener l : listeners) {
-			l.newTorrentsListAvailable(copyof);
-		}
-	}
+        ImmutableList<RSSTorrent> copyof = ImmutableList.copyOf(t);
+        for (RSSUpdateListener l : listeners) {
+            l.newTorrentsListAvailable(copyof);
+        }
+    }
 
-	private Collection<RSSTorrent> getNewTorrents() {
+    private Collection<RSSTorrent> getNewTorrents() {
 
-		Collection<RSSTorrent> torrents = Lists.newArrayList();
-		// First create a new XMLInputFactory
-		XMLInputFactory inputFactory = XMLInputFactory.newInstance();
-		try (InputStream in = url.openStream()) {
+        Collection<RSSTorrent> torrents = Lists.newArrayList();
+        // First create a new XMLInputFactory
+        XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+        try (InputStream in = url.openStream()) {
 
-			XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+            XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
 
-			while (eventReader.hasNext()) {
-				// TODO Something with the header?
-				XMLEvent nextEvent = eventReader.nextEvent();
-				if (nextEvent.isStartElement()) {
-					String lp = nextEvent.asStartElement().getName().getLocalPart();
-					if (is(CHAN, nextEvent)) {
-						break;
-					}
-					System.out.println(lp);
-				}
-			}
+            while (eventReader.hasNext()) {
+                // TODO Something with the header?
+                XMLEvent nextEvent = eventReader.nextEvent();
+                if (nextEvent.isStartElement()) {
+                    String lp = nextEvent.asStartElement().getName().getLocalPart();
+                    if (is(CHAN, nextEvent)) {
+                        break;
+                    }
+                    System.out.println(lp);
+                }
+            }
 
-			// read the XML document
-			while (eventReader.hasNext()) {
-				XMLEvent event = eventReader.nextEvent();
-				if (event.isStartElement()) {
-					if (is(ITEM, event)) {
-						Failable<RSSTorrent> makeTorrent = makeTorrent(eventReader);
-						if (makeTorrent.isFailure()) {
-							logger.debug("Problem parsing feed: " + makeTorrent.getReason());
-							continue;
-						}
-						torrents.add(makeTorrent.get());
-						continue;
-					}
-				}
-				if (event.isEndElement()) {
-					if (is(CHAN, event)) {
-						break;
-					}
-				}
-				throw new IllegalStateException("Torrent item not fully parsed.  Event was " + event);
-			}
-		} catch (XMLStreamException | IOException e) {
-			logger.error("Problem reading RSS feed: " + e.getMessage());
-			return Collections.emptyList();
-		}
-		return torrents;
-	}
+            // read the XML document
+            while (eventReader.hasNext()) {
+                XMLEvent event = eventReader.nextEvent();
+                if (event.isStartElement()) {
+                    if (is(ITEM, event)) {
+                        Failable<RSSTorrent> makeTorrent = makeTorrent(eventReader);
+                        if (makeTorrent.isFailure()) {
+                            logger.debug("Problem parsing feed: " + makeTorrent.getReason());
+                            continue;
+                        }
+                        torrents.add(makeTorrent.get());
+                        continue;
+                    }
+                    if (is("description", event)) {
+                        continue;
+                    }
+                }
+                if (event.isEndElement()) {
+                    if (is(CHAN, event)) {
+                        break;
+                    }
+                }
+                throw new IllegalStateException("Torrent item not fully parsed.  Event was " + event);
+            }
+        } catch (XMLStreamException | IOException e) {
+            logger.error("Problem reading RSS feed: " + e.getMessage());
+            return Collections.emptyList();
+        }
+        return torrents;
+    }
 
-	private Failable<RSSTorrent> makeTorrent(XMLEventReader eventReader) throws XMLStreamException {
-		DefaultRSSTorrent.Builder b = DefaultRSSTorrent.builder();
+    private Failable<RSSTorrent> makeTorrent(XMLEventReader eventReader) throws XMLStreamException {
+        DefaultRSSTorrent.Builder b = DefaultRSSTorrent.builder();
 
-		if (!preRead(eventReader)) {
-			return EXPECTED_START_TAG;
-		}
-		XMLEvent nextEvent = eventReader.nextEvent();
-		b.name(nextEvent.asCharacters().getData());
-		readLong(b, eventReader, NAME_EXT);
-		if (!preRead(eventReader)) {
-			return EXPECTED_START_TAG;
-		}
-		b.link(eventReader.nextEvent().asCharacters().getData());
-		readLong(b, eventReader, LINK_EXT);
-		if (!preRead(eventReader)) {
-			return EXPECTED_START_TAG;
-		}
-		b.date(eventReader.nextEvent().asCharacters().getData());
-		postRead(eventReader);
-		if (!preRead(eventReader)) {
-			return EXPECTED_START_TAG;
-		}
-		b.description(eventReader.nextEvent().asCharacters().getData());
-		readLong(b, eventReader, DESC_EXT);
-		postRead(eventReader);
-		return Failable.ofSuccess((RSSTorrent) b.build());
-	}
+        if (!preRead(eventReader)) {
+            return EXPECTED_START_TAG;
+        }
+        XMLEvent nextEvent = eventReader.nextEvent();
+        b.name(nextEvent.asCharacters().getData());
+        readLong(b, eventReader, NAME_EXT);
+        if (!preRead(eventReader)) {
+            return EXPECTED_START_TAG;
+        }
+        b.link(eventReader.nextEvent().asCharacters().getData());
+        readLong(b, eventReader, LINK_EXT);
+        if (!preRead(eventReader)) {
+            return EXPECTED_START_TAG;
+        }
+        b.date(eventReader.nextEvent().asCharacters().getData());
+        postRead(eventReader);
+        if (!preRead(eventReader)) {
+            return EXPECTED_START_TAG;
+        }
+        b.description(eventReader.nextEvent().asCharacters().getData());
+        readLong(b, eventReader, DESC_EXT);
+        postRead(eventReader);
+        return Failable.ofSuccess((RSSTorrent) b.build());
+    }
 
-	private void readLong(DefaultRSSTorrent.Builder b, XMLEventReader eventReader, Extension linkExt)
-			throws XMLStreamException {
-		while (true) {
-			XMLEvent nextEvent2 = eventReader.nextEvent();
-			if (nextEvent2.isEndElement()) {
-				break;
-			}
-			String data = nextEvent2.asCharacters().getData();
-			linkExt.apply(b, data);
-		}
-	}
+    private void readLong(DefaultRSSTorrent.Builder b, XMLEventReader eventReader, Extension linkExt)
+            throws XMLStreamException {
+        while (true) {
+            XMLEvent nextEvent2 = eventReader.nextEvent();
+            if (nextEvent2.isEndElement()) {
+                break;
+            }
+            String data = nextEvent2.asCharacters().getData();
+            linkExt.apply(b, data);
+        }
+    }
 
-	private boolean preRead(XMLEventReader eventReader) throws XMLStreamException {
-		XMLEvent nextEvent = eventReader.nextEvent();
-		if (nextEvent.isStartElement()) {
-			return true;
-		}
-		return false;
-		// throw new FormatChangedException("Expected start tag");
-	}
+    private boolean preRead(XMLEventReader eventReader) throws XMLStreamException {
+        XMLEvent nextEvent = eventReader.nextEvent();
+        if (nextEvent.isStartElement()) {
+            return true;
+        }
+        return false;
+        // throw new FormatChangedException("Expected start tag");
+    }
 
-	private void postRead(XMLEventReader eventReader) throws XMLStreamException {
-		XMLEvent nextEvent = eventReader.nextEvent();
-		if (nextEvent.isEndElement()) {
-			return;
-		}
+    private void postRead(XMLEventReader eventReader) throws XMLStreamException {
+        XMLEvent nextEvent = eventReader.nextEvent();
+        if (nextEvent.isEndElement()) {
+            return;
+        }
 		// throw new FormatChangedException("Expected end tag but was " +
-		// nextEvent);
-	}
+        // nextEvent);
+    }
 
-	private boolean is(String constant, XMLEvent event) {
-		Preconditions.checkNotNull(constant);
-		if (event.isStartElement()) {
-			StartElement asStartElement = event.asStartElement();
-			return doIs(constant, asStartElement.getName());
-		}
-		if (event.isEndElement()) {
-			EndElement asEnd = event.asEndElement();
-			return doIs(constant, asEnd.getName());
-		}
-		throw new RuntimeException("Unexpected tag " + event);
-	}
+    private boolean is(String constant, XMLEvent event) {
+        Preconditions.checkNotNull(constant);
+        if (event.isStartElement()) {
+            StartElement asStartElement = event.asStartElement();
+            return doIs(constant, asStartElement.getName());
+        }
+        if (event.isEndElement()) {
+            EndElement asEnd = event.asEndElement();
+            return doIs(constant, asEnd.getName());
+        }
+        throw new RuntimeException("Unexpected tag " + event);
+    }
 
-	private boolean doIs(String constant, QName name) {
-		String localPart = name.getLocalPart();
-		return constant.equals(localPart);
-	}
+    private boolean doIs(String constant, QName name) {
+        String localPart = name.getLocalPart();
+        return constant.equals(localPart);
+    }
 
-	@Override
-	public void addUpdateListener(RSSUpdateListener listener) {
-		Preconditions.checkNotNull(listener);
-		this.listeners.add(listener);
-	}
+    @Override
+    public void addUpdateListener(RSSUpdateListener listener) {
+        Preconditions.checkNotNull(listener);
+        this.listeners.add(listener);
+    }
 
 }
